70b446f8c4b0908ace79988c245bc05212c91e47
Nick DeRobertis <derobertisna@ufl.edu>
2017-07-12 10:28:19 -0400

    0.8.2: modified pandas.long_to_wide to allow a list for colindex

--- setup.py
+++ setup.py
@@ -30,7 +30,8 @@ setup(name='Dero',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5'
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
         ],
        install_requires=[
         'pandas',
