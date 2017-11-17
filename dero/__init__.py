f635ca14814832b36af6ccd9e21434f2207f1b01
Nick DeRobertis <whoopnip@gmail.com>
2017-11-17 09:48:43 -0500

    Fixes to df_to_pdf_and_move for string formats with csv outmethod. Added as_document parameter which can be set to False so that table can be included in a paper

--- dero/__init__.py
+++ dero/__init__.py
@@ -1,6 +1,6 @@
 
-__version__ = "0.8.2"
-__version_info__ = (0, 8, 2)
+__version__ = "0.8.5"
+__version_info__ = (0, 8, 5)
 
 #Provided for backwards compatibility with old code. In the old code, add to the top:
 #import builtins
