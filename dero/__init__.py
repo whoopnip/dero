969e1ac7ec6faf1e800bf3a66405c00fc97cfebc
Nick DeRobertis <whoopnip@gmail.com>
2017-12-03 19:26:41 -0500

    0.8.11: Update filepaths for tradedays

--- dero/__init__.py
+++ dero/__init__.py
@@ -1,6 +1,6 @@
 
-__version__ = "0.8.10"
-__version_info__ = (0, 8, 10)
+__version__ = "0.8.11"
+__version_info__ = (0, 8, 11)
 
 #Provided for backwards compatibility with old code. In the old code, add to the top:
 #import builtins
