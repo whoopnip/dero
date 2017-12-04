dbd0e57c92b9cb71d2ba2a56209f0d42e12cd294
Nick DeRobertis <whoopnip@gmail.com>
2017-12-04 08:36:24 -0500

    0.8.16: Fixed an error in factor_reg_by for not using new factors

--- first_time_upload.bat
+++ first_time_upload.bat
@@ -1,5 +1,5 @@
 
 python setup.py sdist bdist_wheel
-twine register dist\Dero-0.8.15*
-twine upload dist\Dero-0.8.15*
+twine register dist\Dero-0.8.16*
+twine upload dist\Dero-0.8.16*
 pause
--- upload.bat
+++ upload.bat
@@ -1,4 +1,4 @@
 
 python setup.py sdist bdist_wheel
-twine upload dist\Dero-0.8.15*
+twine upload dist\Dero-0.8.16*
 pause
