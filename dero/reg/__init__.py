9606767628eb40d1a813a78b0a3ac22eda0942f4
Nick DeRobertis <whoopnip@gmail.com>
2017-12-15 11:36:09 -0500

    Add RegressionSetArgs as a way of organizing and maintaining regression sets

--- dero/reg/__init__.py
+++ dero/reg/__init__.py
@@ -3,5 +3,6 @@ from .iter import reg_for_each_combo, reg_for_each_xvar_set, reg_for_each_combo_
 from .reg import reg
 from .summarize import produce_summary
 from .select import select_models
+from .args import RegressionSetArgs
 
 
