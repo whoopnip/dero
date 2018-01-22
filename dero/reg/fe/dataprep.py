afc02ce6d4380663326be05317d6172cb580e3be
Nick DeRobertis <whoopnip@gmail.com>
2018-01-22 09:42:17 -0500

    0.9.4: Fixed error with fixed effect regressions where dummy group was not being excluded

--- dero/reg/fe/dataprep.py
+++ dero/reg/fe/dataprep.py
@@ -14,7 +14,7 @@ def fixed_effects_reg_df_and_cols_dict(df, fe_vars):
     return df, fe_cols_dict
 
 def _fixed_effects_reg_df_and_cols(df, fe_var):
-    dummies = pd.get_dummies(df[fe_var])
+    dummies = pd.get_dummies(df[fe_var]).iloc[:,1:] #drop first dummy, this will be the excluded group
     dummy_cols = [col for col in dummies.columns]
     return pd.concat([df, dummies], axis=1).drop(fe_var, axis=1), dummy_cols
 
