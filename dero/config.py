0b5c42e7d46de6b9e7301a244c44fa8e867ab9c6
Nick DeRobertis <whoopnip@gmail.com>
2018-02-10 23:07:45 -0500

    Add ETF Path Function

--- dero/config.py
+++ dero/config.py
@@ -9,15 +9,15 @@ def data_path(subpath):
     return os.path.join(data_filepath, subpath)
 
 def data_path_func(subpath):
-    return _path_func(subpath, data_path)
+    return path_func(subpath, data_path)
 
 def dropbox_path(subpath):
     return os.path.join(dropbox_filepath, subpath)
 
 def dropbox_path_func(subpath):
-    return _path_func(subpath, dropbox_path)
+    return path_func(subpath, dropbox_path)
 
-def _path_func(subpath, path_func):
+def path_func(subpath, path_func):
     new_path = path_func(subpath)
     new_path_func = partial(os.path.join, new_path)
     return new_path_func
@@ -25,3 +25,4 @@ def _path_func(subpath, path_func):
 #### Project paths
 
 etf_project_path = dropbox_path_func('UF\Andy\ETF Project')
+etf_path_func = lambda subpath: path_func(subpath, etf_project_path)
