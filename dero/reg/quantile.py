60af674c12cf956eae4fecbbd184bf254970e203
Nick DeRobertis <whoopnip@gmail.com>
2018-03-26 20:57:42 -0400

    Add convenience function for running quantile regressions then plotting. Added docstrings in quantile

--- dero/reg/quantile.py
+++ dero/reg/quantile.py
@@ -57,7 +57,54 @@ def quantile_reg(df, yvar, xvars, q=0.5, robust=True, cluster=False, cons=True, 
         return result
 
 
+def reg_for_each_quantile_output_plot(main_iv, *reg_args, num_quantiles=8, main_iv_label=None, outpath=None,
+                                      clear_figure=True, **reg_kwargs):
+    """
+    Creates a plot of effect of main_iv on yvar at different quantiles. To be used after
+    reg_for_each_quantile_produce_result_df
+
+    :param main_iv: str, column name of independent variable of interest
+    :param reg_args: see quantile_reg for args
+    :param num_quantiles: number of quantile regressions to run. will be spaced evenly. higher numbers
+                          produce smoother graphs but take longer to run
+    :param main_iv_label: str, label of independent variable of interest
+    :param outpath: str, filepath to output figure. must include matplotlib supported extension such as .pdf or .png
+    :param clear_figure: bool, True wipe memory of matplotlib figure after running function
+    :param reg_kwargs: see quantile_reg for kwargs
+    :return: result_df from reg_for_each_quantile_produce_result_df
+    """
+
+    main_iv_label = _validate_quantile_reg_and_plot_args(main_iv, main_iv_label)
+
+    result_df = reg_for_each_quantile_produce_result_df(
+        main_iv,
+        *reg_args,
+        num_quantiles=num_quantiles,
+        **reg_kwargs
+    )
+
+    quantile_plot_from_quantile_result_df(
+        result_df,
+        reg_args[1], #yvar
+        main_iv_label,
+        outpath=outpath,
+        clear_figure=clear_figure
+    )
+
+    return result_df
+
 def reg_for_each_quantile_produce_result_df(main_iv, *reg_args, num_quantiles=8, **reg_kwargs):
+    """
+    Produce result DataFrame of running multiple quantile regressions spaced out between the
+    (0,1) interval
+
+    :param main_iv: str, column name of independent variable of interest
+    :param reg_args: see quantile_reg for args
+    :param num_quantiles: number of quantile regressions to run. will be spaced evenly. higher numbers
+                          produce smoother graphs but take longer to run
+    :param reg_kwargs: see quantile_reg for kwargs
+    :return:
+    """
     quantiles = _create_quantiles_from_num_quantiles(num_quantiles)
     results = [(quantile_reg(*reg_args, q=q, **reg_kwargs), q) for q in quantiles]
     main_iv = _set_regressor_order([main_iv], reg_kwargs)[0]
@@ -81,7 +128,18 @@ def _create_quantiles_from_num_quantiles(num_quantiles):
     return np.arange(quant_lower_bound, quant_upper_bound, quant_lower_bound)
 
 
-def quantile_plot_from_quantile_result_df(result_df, yvar, main_iv, outpath=None):
+def quantile_plot_from_quantile_result_df(result_df, yvar, main_iv, outpath=None, clear_figure=True):
+    """
+    Creates a plot of effect of main_iv on yvar at different quantiles. To be used after
+    reg_for_each_quantile_produce_result_df
+
+    :param result_df: pd.DataFrame, result from reg_for_each_quantile_produce_result_df
+    :param yvar: str, label of dependent variable
+    :param main_iv: str, label of independent variable of interest
+    :param outpath: str, filepath to output figure. must include matplotlib supported extension such as .pdf or .png
+    :param clear_figure: bool, True wipe memory of matplotlib figure after running function
+    :return:
+    """
     p1 = plt.plot(result_df.q, result_df.b, color='black', label='Quantile Regression Slope')
     p2 = plt.plot(result_df.q, result_df.ub, linestyle='dotted', color='black',
                   label='95% Confidence Interval Lower Bound')
@@ -97,6 +155,23 @@ def quantile_plot_from_quantile_result_df(result_df, yvar, main_iv, outpath=None
     else:
         plt.show()
 
+    if clear_figure:
+        plt.clf()
+
 
 def _is_quantile_reg_str(reg_str):
     return reg_str in ('quantile', 'quant', 'q', 'quantile reg', 'quantile_reg', 'quant_reg', 'quantreg', 'qreg', 'quantile regression')
+
+def _validate_quantile_reg_and_plot_args(main_iv, main_iv_label):
+    main_iv_label = _set_main_iv_label(main_iv, main_iv_label)
+
+    return main_iv_label
+
+def _set_main_iv_label(main_iv, main_iv_label):
+    """
+    Set default main iv label as main iv
+    """
+    if main_iv_label is None:
+        return main_iv
+    else:
+        return main_iv_label
