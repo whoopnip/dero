8d884dd6ea35c9f9081ce5a515e26b1342779f8b
Nick DeRobertis <whoopnip@gmail.com>
2018-06-20 08:08:14 -0400

    More docstrings, examples in docstrings, import what is needed in __init__

--- dero/reg/__init__.py
+++ dero/reg/__init__.py
@@ -1,8 +1,16 @@
-from .iter import reg_for_each_combo, reg_for_each_xvar_set, reg_for_each_combo_select_and_produce_summary, \
-    reg_for_each_xvar_set_and_produce_summary, reg_for_each_yvar_and_produce_summary
+from .iter import (
+    reg_for_each_combo,
+    reg_for_each_xvar_set,
+    reg_for_each_combo_select_and_produce_summary,
+    reg_for_each_xvar_set_and_produce_summary,
+    reg_for_each_yvar,
+    reg_for_each_yvar_and_produce_summary,
+    reg_for_each_lag,
+    reg_for_each_lag_and_produce_summary
+)
 from .reg import reg
 from .summarize import produce_summary
 from .select import select_models
 from .args import RegressionSetArgs
-
+from dero.reg.quantile import quantile_reg
 
