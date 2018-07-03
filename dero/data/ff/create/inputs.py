232703a56878196a0743836fdfb79f36b2cc6660
Nick DeRobertis <whoopnip@gmail.com>
2018-07-03 04:38:34 -0400

    Create FF factors: bug fix for standard FF factors

--- dero/data/ff/create/inputs.py
+++ dero/data/ff/create/inputs.py
@@ -14,16 +14,22 @@ def _standardize_custom_args(custom_labels: DictofStrsandStrLists = None,
                              ) -> Tuple[DictofStrsandStrLists, GroupvarNgroupsDict, TwoStrTupleList, StrBoolDict]:
 
     # Replace variables with portfolio variables
-    custom_labels = {_other_groupvar_portname(key): value for key, value in custom_labels.items()}
-    custom_low_minus_high_dict = {_other_groupvar_portname(key): value for key, value in custom_low_minus_high_dict.items()}
+    if custom_labels is not None:
+        custom_labels = {_other_groupvar_portname(key): value for key, value in custom_labels.items()}
 
-    new_custom_pairings = []
-    for pairing in custom_pairings:
-        new_custom_pairings.append(
-            (
-                _other_groupvar_portname(pairing[0]),
-                _other_groupvar_portname(pairing[1])
+    if custom_low_minus_high_dict is not None:
+        custom_low_minus_high_dict = {_other_groupvar_portname(key): value for key, value in custom_low_minus_high_dict.items()}
+
+    if custom_pairings is not None:
+        new_custom_pairings = []
+        for pairing in custom_pairings:
+            new_custom_pairings.append(
+                (
+                    _other_groupvar_portname(pairing[0]),
+                    _other_groupvar_portname(pairing[1])
+                )
             )
-        )
+    else:
+        new_custom_pairings = None
 
     return custom_labels, custom_groupvar_ngroups_dict, new_custom_pairings, custom_low_minus_high_dict
