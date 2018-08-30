e558f5886337db81bf49068d545ed5e1f2c48cf4
Alden Lobo <aldenlobo@gmail.com>
2018-08-29 21:25:50 -0400

    Updated the conf.py file to point to appropriate directory paths

--- docs/conf.py
+++ docs/conf.py
@@ -15,9 +15,9 @@
 import os
 import sys
 # sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('..'))
+# sys.path.insert(0, os.path.abspath('..'))
 # sys.path.insert(0, os.path.abspath('../'))
-# sys.path.append(os.path.join(os.path.dirname(__name__), '..'))
+sys.path.append(os.path.join(os.path.dirname(__name__), '..'))
 # sys.path.insert(0, "E:\\University of Florida\\Joost\\Dero")
 
 # -- Project information -----------------------------------------------------
