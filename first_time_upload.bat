994be8ef756da04334d047bab31e1846ad1c3e04
Nick DeRobertis <whoopnip@gmail.com>
2018-09-02 14:01:30 -0400

    0.14.0: Linux support. Optimize df. Load sources. Page numbers, header footer for latex documents. Describe df. Manager bug fixes for isinstance. Linearmodels fixed effects. Outliers module. Missing data module. Various bug fixes

--- first_time_upload.bat
+++ first_time_upload.bat
@@ -1,5 +1,5 @@
 
 python setup.py sdist bdist_wheel
-twine register dist\Dero-0.13.0*
-twine upload dist\Dero-0.13.0*
+twine register dist\Dero-0.14.0*
+twine upload dist\Dero-0.14.0*
 pause
--- upload.bat
+++ upload.bat
@@ -1,4 +1,4 @@
 
 python setup.py sdist bdist_wheel
-twine upload dist\Dero-0.13.0*
+twine upload dist\Dero-0.14.0*
 pause
