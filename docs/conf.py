d804faba0134e802d4b92141e5d3187c1894ab63
Alden Lobo <aldenlobo@gmail.com>
2018-09-03 11:42:18 -0400

    Added the class Mock & added tkinter to the list

--- docs/conf.py
+++ docs/conf.py
@@ -190,5 +190,13 @@ autodoc_mock_imports = [
     'any_reg', 'numpy', 'dateutil', 'selenium' 
 ]
 html_theme = 'default'
-import matplotlib
-matplotlib.use('agg')
+
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+MOCK_MODULES = ['tkinter']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
