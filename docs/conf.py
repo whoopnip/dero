4c8c213abf06a7aad6199a439ca373be760781f8
Alden Lobo <aldenlobo@gmail.com>
2018-09-03 13:10:38 -0400

    Added tkinter to the mock list

--- docs/conf.py
+++ docs/conf.py
@@ -186,7 +186,7 @@ todo_include_todos = True
 autodoc_mock_imports = [
     'unidecode', 'sas7bdat', 'pandasql', 'pandastable', 'statsmodels', 
     'pdfrw', 'linearmodels', 'matplotlib', 'pandas', 'sympy', 
-    'any_reg', 'numpy', 'dateutil', 'selenium'
+    'any_reg', 'numpy', 'dateutil', 'selenium', 'tkinter'
 ]
 html_theme = 'default'
 
