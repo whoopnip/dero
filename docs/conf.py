cf7ae69af721e5ec263b7ab0ff25c69ce527fc99
Alden Lobo <aldenlobo@gmail.com>
2018-09-03 13:08:46 -0400

    Removed tkinter mocking

--- docs/conf.py
+++ docs/conf.py
@@ -190,12 +190,12 @@ autodoc_mock_imports = [
 ]
 html_theme = 'default'
 
-from unittest.mock import MagicMock
+# from unittest.mock import MagicMock
 
-class Mock(MagicMock):
-    @classmethod
-    def __getattr__(cls, name):
-        return MagicMock()
+# class Mock(MagicMock):
+#     @classmethod
+#     def __getattr__(cls, name):
+#         return MagicMock()
 
-MOCK_MODULES = ['tkinter']
-sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+# MOCK_MODULES = ['tkinter']
+# sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
