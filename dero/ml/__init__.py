b4ced9e0a33a24deb50b57bb3e83b10679f64261
Nick DeRobertis <whoopnip@gmail.com>
2019-09-22 20:18:11 -0400

    Add docstrings for packages

---
+++ dero/ml/__init__.py
@@ -0,0 +1,4 @@
+"""
+A framework to assist in carrying out the standard machine learning workflow: split data into training, validation,
+and test samples, train and evaluate models, and run final predictions with the selected model.
+"""
---
+++ dero/modeler/__init__.py
@@ -0,0 +1,3 @@
+"""
+A modeling framework where the user provides equations and the model is solved and summarized automatically.
+"""
